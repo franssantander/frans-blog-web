cfb6a0bf546b8b45afc8538a7d1f847a71f74c2f
franssantander <santanderfrancisbeam@gmail.com>
2026-02-26 00:13:21 +0800

    chore: added favicon

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "thedankoe.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
